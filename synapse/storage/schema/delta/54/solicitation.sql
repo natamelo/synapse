fa2c798b026688bd0e1253588f6c6c807182c2e9
dalesEwerton <dales.fragoso@ccc.ufcg.edu.br>
2019-05-03 14:22:18 -0300

    Change solicitation table to accept new fields from sage call

--- synapse/storage/schema/delta/54/solicitation.sql
+++ synapse/storage/schema/delta/54/solicitation.sql
@@ -2,7 +2,7 @@
  *  Synapse Cteep
  */
 
-CREATE TABLE IF NOT EXISTS solicitations ( id INTEGER PRIMARY KEY, sender_user_id TEXT,event_id TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS solicitations ( id INTEGER PRIMARY KEY, sender_user_id TEXT,event_id TEXT,
                                            status TEXT NOT NULL, action TEXT, substation_code TEXT,
-                                           equipment_type TEXT, equipment_code TEXT,UNIQUE (event_id) );
+                                           equipment_type TEXT, equipment_code TEXT);
 CREATE INDEX IF NOT EXISTS solicitations_event ON solicitations ( event_id );
